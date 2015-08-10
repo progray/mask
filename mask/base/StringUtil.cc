2cc3b8d69e0cfc3255bc0122c833efcf83ff0283
Junlong Xie <decimalbell@gmail.com>
2015-08-10 13:06:19 +0800

    replace snprintf with rapidjson itoa see https://github.com/miloyip/itoa-benchmark

--- mask/base/StringUtil.cc
+++ mask/base/StringUtil.cc
@@ -1,69 +1,56 @@
 #include <mask/base/StringUtil.h>
 
-#ifndef __STDC_FORMAT_MACROS
-#define __STDC_FORMAT_MACROS
-#endif
-
-#include <inttypes.h>
-#include <stdio.h>
+#include <rapidjson/internal/itoa.h>
 
 namespace mask
 {
 
 muduo::string toString(uint8_t value)
 {
-  char buf[32];
-  snprintf(buf, sizeof buf, "%"PRIu8, value);
-  return buf;
+  return toString(static_cast<uint32_t>(value));
 }
 
 muduo::string toString(uint16_t value)
 {
-  char buf[32];
-  snprintf(buf, sizeof buf, "%"PRIu16, value);
-  return buf;
+  return toString(static_cast<uint32_t>(value));
 }
 
 muduo::string toString(uint32_t value)
 {
   char buf[32];
-  snprintf(buf, sizeof buf, "%"PRIu32, value);
+  rapidjson::internal::u32toa(value, buf);
   return buf;
 }
 
 muduo::string toString(uint64_t value)
 {
   char buf[32];
-  snprintf(buf, sizeof buf, "%"PRIu64, value);
+  rapidjson::internal::u64toa(value, buf);
   return buf;
 }
 
 muduo::string toString(int8_t value)
 {
-  char buf[32];
-  snprintf(buf, sizeof buf, "%"PRId8, value);
-  return buf;
+  return toString(static_cast<int32_t>(value));
 }
 
 muduo::string toString(int16_t value)
 {
-  char buf[32];
-  snprintf(buf, sizeof buf, "%"PRId16, value);
-  return buf;
+  return toString(static_cast<int32_t>(value));
 }
 
 muduo::string toString(int32_t value)
 {
   char buf[32];
-  snprintf(buf, sizeof buf, "%"PRId32, value);
+  rapidjson::internal::i32toa(value, buf);
   return buf;
 }
 
 muduo::string toString(int64_t value)
 {
   char buf[32];
-  snprintf(buf, sizeof buf, "%"PRId64, value);
+  rapidjson::internal::i64toa(value, buf);
   return buf;
 }
 
-}
+} // mask
